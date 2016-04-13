647fb76046aa19247697bc20f8a969bbe14cac4c
xiaoqi1102 <705764065@qq.com>
2016-04-13 13:24:53 +0800

    打包入口

---
+++ main.js
@@ -0,0 +1,9 @@
+
+/**
+ * Created by yzsoft on 16/4/12.
+ */
+import React from 'react'
+import {render} from 'react-dom';
+import App from './redux_lesson/lession_0'
+let rootDocument=document.getElementById('content')
+render(<App/>,rootDocument);
